cc5f0b8c2908e7df05279340b6d03fd62549fda9
Mitanshu Bhasin <mfskufgu@gmail.com>
2026-03-06 17:34:03 +0530

    chore: remove final env.js comment from firebase-config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,12 @@
 // Firebase Configuration
-// Uses variables loaded from env.js (which should be added before this script)
 const firebaseConfig = {
-  apiKey: "AIzaSyBHQF5cUBujrCJqOqybEUIeanTCbHYpMWU",
-  authDomain: "expense-manager-ec149.firebaseapp.com",
-  projectId: "expense-manager-ec149",
-  storageBucket: "expense-manager-ec149.firebasestorage.app",
-  messagingSenderId: "868468480650",
-  appId: "1:868468480650:web:484a4e831724a8112feb73",
-  measurementId: "G-1K8CS62VLR"
+    apiKey: "AIzaSyBHQF5cUBujrCJqOqybEUIeanTCbHYpMWU",
+    authDomain: "expense-manager-ec149.firebaseapp.com",
+    projectId: "expense-manager-ec149",
+    storageBucket: "expense-manager-ec149.firebasestorage.app",
+    messagingSenderId: "868468480650",
+    appId: "1:868468480650:web:484a4e831724a8112feb73",
+    measurementId: "G-1K8CS62VLR"
 };
 
 // Initialize Firebase
