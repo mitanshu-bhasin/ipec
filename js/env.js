79945d7a4789d47b87542f4000adc565740605ed
Mitanshu Bhasin <mfskufgu@gmail.com>
2026-03-06 17:18:58 +0530

    Refactor: Added Enterprise UI, Security Policy, and Funding options

---
+++ js/env.js
@@ -0,0 +1,15 @@
+// env.js
+// This file contains environment variables and MUST NOT be committed to version control.
+window.ENV = {
+    GEMINI_API_KEY: 'AIzaSyDKy8-WdO1gqlDr6wDjHjdfI9D2nknTF14',
+    FIREBASE_API_KEY: "AIzaSyBHQF5cUBujrCJqOqybEUIeanTCbHYpMWU",
+    FIREBASE_AUTH_DOMAIN: "expense-manager-ec149.firebaseapp.com",
+    FIREBASE_PROJECT_ID: "expense-manager-ec149",
+    FIREBASE_STORAGE_BUCKET: "expense-manager-ec149.firebasestorage.app",
+    FIREBASE_MESSAGING_SENDER_ID: "868468480650",
+    FIREBASE_APP_ID: "1:868468480650:web:484a4e831724a8112feb73",
+    FIREBASE_MEASUREMENT_ID: "G-1K8CS62VLR",
+    IMAGEKIT_URL: "https://upload.imagekit.io/api/v1/files/upload",
+    IMAGEKIT_PUBLIC_KEY: "public_A7ZHxCiIBfl/fxzXlk1tXUUR+B4=",
+    IMAGEKIT_PRIVATE_KEY: "private_m7Ie0ZXD/4WpBlly116odVbWhhM="
+};
